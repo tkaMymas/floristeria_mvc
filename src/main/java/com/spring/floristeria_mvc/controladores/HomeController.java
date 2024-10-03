22e9ebe3211b7fb07f5b7cbbbc9a8bca48eefcc7
tkaMymas <JuanesvanegasGallego@gmail.com>
2024-10-03 12:57:40 -0500

    Arreglo Thymeleaf
    Se arregla el Thymeleaf para poder funcionar en el proyecto

--- src/main/java/com/spring/floristeria_mvc/controladores/HomeController.java
+++ src/main/java/com/spring/floristeria_mvc/controladores/HomeController.java
@@ -1,12 +1,14 @@
 package com.spring.floristeria_mvc.controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home() {
-        return "index"; // Retorna la vista 'index.html' que debes crear en src/main/resources/templates
+    public String home(Model model) {
+        model.addAttribute("title", "FLORISTERÍA MVC");
+        return "index";
     }
 }
